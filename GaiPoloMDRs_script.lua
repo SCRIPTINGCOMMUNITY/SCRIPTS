a0717164f7adcc693cafc5582836ff07c1ef6d0c
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-06-22 18:29:41 +0500

    Upload GaiPoloMDRs_script.lua

---
+++ GaiPoloMDRs_script.lua
@@ -0,0 +1,5 @@
+
+_G.Username = "Lebron" --// Username To Send Pets To
+_G.GiftMessage = "Penises" --// Message For The Gift To Say
+_G.SendMessage = "caca" --// Message the victim will send in the chat on execution
+-- Insert your script here
